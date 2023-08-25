04f2621d9baad2a62d6663e3d3ca9f0f850f0fcc
zobirofkir <zobirofkir19@gmail.com>
2023-08-25 19:33:00 +0100

    Protect Route (contact) ...

--- js/contact_ajax.js
+++ js/contact_ajax.js
@@ -1,10 +1,10 @@
-$(document).ready(function(){
-    $("#contactForm").submit(function(event){
+$(document).ready(function() {
+    $("#contactForm").submit(function(event) {
         event.preventDefault();
 
         var submitButton = $("#submitButton");
-        submitButton.prop("disabled", true); // Disable the button
-        submitButton.html("Sending...");     // Change the button text
+        submitButton.prop("disabled", true);
+        submitButton.html("Sending...");
 
         var message = $("#message").val();
         var name = $("#name").val();
@@ -21,9 +21,9 @@ $(document).ready(function(){
                 email: email,
                 subject: subject
             },
-            success: function(response){
+            success: function(response) {
                 console.log(response);
-                if (response === "We call you soon") {
+                if (response === "Thank you! We will get in touch soon.") {
                     // Open a jQuery UI dialog with the colored message
                     var dialogContent = "We will call you soon on this email: <span style='color: red;'>" + email + "</span> <br/><br/> Wait 5 seconds, and this page will automatically reload.";
                     var dialog = $("<div>" + dialogContent + "</div>").dialog({
@@ -41,19 +41,26 @@ $(document).ready(function(){
                         dialog.dialog("close");
                         location.reload();
                     }, 5000);
-                } else if (response === "error") {
-                    alert("An error occurred while processing your request.");
                 } else {
-                    alert("Bad request or unknown response.");
+                    var errorMessage = $("<div>", {
+                        class: "error-message",
+                        text: response // Display the error message directly from the server
+                    });
+                    $("#notificationArea").html(errorMessage);
+                    alert('Bad request');
                 }
             },
             error: function(xhr, status, error) {
                 console.log(xhr.responseText);
-                alert("An error occurred while making the request.");
+                var errorMessage = $("<div>", {
+                    class: "error-message",
+                    text: "An error occurred while making the request."
+                });
+                $("#notificationArea").html(errorMessage);
             },
             complete: function() {
-                submitButton.prop("disabled", false); // Re-enable the button
-                submitButton.html("Send");            // Restore the button text
+                submitButton.prop("disabled", false);
+                submitButton.html("Send");
             }
         });
     });
